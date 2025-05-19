e2aca8a9e02374a019b1d32f01d18f532e6df087
aayushxrj <aayush.ranjan0904@gmail.com>
2025-05-19 16:14:41 +0530

    feat: GET /api/books

--- src/main/java/com/aayushxrj/books/controller/BookController.java
+++ src/main/java/com/aayushxrj/books/controller/BookController.java
@@ -1,14 +1,40 @@
 package com.aayushxrj.books.controller;
 
+import com.aayushxrj.books.entity.Book;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class BookController {
 
-    @GetMapping("/api-endpoint")
-    public String firstAPI(){
-        return "Hello, Aayush!";
+    private final List<Book> books = new ArrayList<>();
+
+    public BookController(){
+        initialiseBooks();
     }
+
+    private void initialiseBooks(){
+        books.addAll(List.of(
+                new Book("Title one", "Author one", "Science"),
+                new Book("Title two", "Author two", "Science"),
+                new Book("Title three", "Author three", "History"),
+                new Book("Title four", "Author four", "Maths"),
+                new Book("Title five", "Author five", "Maths"),
+                new Book("Title six", "Author six", "Maths")
+        ));
+    }
+
+    @GetMapping("/api/books")
+    public List<Book> getBooks(){
+        return books;
+    }
+
+//    @GetMapping("/api")
+//    public String firstAPI(){
+//        return "Hello, Aayush!";
+//    }
 
 }
