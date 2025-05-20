2dba73d04ad79c10339d1e47ed2ef910ccebd855
aayushxrj <aayush.ranjan0904@gmail.com>
2025-05-20 12:18:07 +0530

    feat : globalException handler

--- src/main/java/com/aayushxrj/books/controller/BookController.java
+++ src/main/java/com/aayushxrj/books/controller/BookController.java
@@ -120,5 +120,15 @@ public class BookController {
 
         return new ResponseEntity<>(bookErrorResponse, HttpStatus.NOT_FOUND);
     }
+    @ExceptionHandler
+    public ResponseEntity<BookErrorResponse> handleException(Exception exc){
+        BookErrorResponse bookErrorResponse = new BookErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "Invalid Request",
+                System.currentTimeMillis()
+        );
+
+        return new ResponseEntity<>(bookErrorResponse, HttpStatus.BAD_REQUEST);
+    }
 
 }
